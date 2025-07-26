ed04189ad46d3a5562a54072b026b464545cd5e8
AmirHossein Advari <amiradvari@gmail.com>
2025-07-27 00:18:49 +0330

    Add conditional imports for Qdrant and Redis services and repositories

--- src/minix/core/service/__init__.py
+++ src/minix/core/service/__init__.py
@@ -1,4 +1,10 @@
+import importlib.util
+
 from .service import Service
 from minix.core.service.sql.sql_service import SqlService
-from minix.core.service.redis.redis_service import RedisService
-from minix.core.service.qdrant.qdrant_service import QdrantService
+
+if importlib.util.find_spec('qdrant_client'):
+    from minix.core.service.qdrant.qdrant_service import QdrantService
+
+if importlib.util.find_spec('redis'):
+    from minix.core.service.redis.redis_service import RedisService
