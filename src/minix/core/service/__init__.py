aee0eeb380df77d6dcad922ff0df777c6b0851b4
AmirHossein Advari <amiradvari@gmail.com>
2025-07-27 00:27:24 +0330

    Add Redis service and repository imports; bump version to 0.1.17

--- src/minix/core/service/__init__.py
+++ src/minix/core/service/__init__.py
@@ -2,9 +2,8 @@ import importlib.util
 
 from .service import Service
 from minix.core.service.sql.sql_service import SqlService
+from minix.core.service.redis.redis_service import RedisService
 
 if importlib.util.find_spec('qdrant-client'):
     from minix.core.service.qdrant.qdrant_service import QdrantService
 
-if importlib.util.find_spec('redis'):
-    from minix.core.service.redis.redis_service import RedisService
