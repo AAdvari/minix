db175ae0578011ebe23f4ec376360174d257ed82
AmirHossein Advari <amiradvari@gmail.com>
2025-07-27 00:22:51 +0330

    Fix import check for Qdrant service to use correct package name

--- src/minix/core/service/__init__.py
+++ src/minix/core/service/__init__.py
@@ -3,7 +3,7 @@ import importlib.util
 from .service import Service
 from minix.core.service.sql.sql_service import SqlService
 
-if importlib.util.find_spec('qdrant_client'):
+if importlib.util.find_spec('qdrant-client'):
     from minix.core.service.qdrant.qdrant_service import QdrantService
 
 if importlib.util.find_spec('redis'):
