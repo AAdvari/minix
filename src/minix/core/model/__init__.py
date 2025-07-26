51746601e9701f2b5ba6768d5e48cba94ce1d88c
AmirHossein Advari <amiradvari@gmail.com>
2025-07-27 00:09:33 +0330

    Add conditional import for MlflowModel and bump version to 0.1.13 in pyproject.toml

--- src/minix/core/model/__init__.py
+++ src/minix/core/model/__init__.py
@@ -1,4 +1,8 @@
+import importlib.util
 from .model import Model
-from .mlflow_model import MlflowModel
 from .model_registry import ModelRegistry
 from .embedding_model import EmbeddingModel
+
+if importlib.util.find_spec('mlflow') and importlib.util.find_spec('torch'): # Check if mlflow is installed
+     from .mlflow_model import MlflowModel
+
