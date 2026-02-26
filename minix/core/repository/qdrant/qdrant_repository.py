e25953c457d147152fb99c9610755f0370623287
AmirHossein Advari <aad@adlas.at>
2026-02-26 11:55:06 +0330

    fix: initialize client after ensuring connection in qdrant_repository.py

--- minix/core/repository/qdrant/qdrant_repository.py
+++ minix/core/repository/qdrant/qdrant_repository.py
@@ -16,10 +16,10 @@ class QdrantRepository(Repository[T], ABC):
         super().__init__(entity)
         self.entity = entity
         self.connector = qdrant_connector
-        self.client = self.connector.client
         ## check if collection exists, if not create it
         if qdrant_connector.client is None:
             self.connector.connect()
+        self.client = self.connector.client
         collections = self.client.get_collections().collections
         collection_names = [collection.name for collection in collections]
         if not self.entity.collection() in collection_names:
